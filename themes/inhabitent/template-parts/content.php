494b67f1eeba673f93a634daf0a74355ddb6db9a
Evan Gray <evanlawgray@gmail.com>
2017-02-20 13:01:05 -0800

    Add and style journal read more buttons.

--- themes/inhabitent/template-parts/content.php
+++ themes/inhabitent/template-parts/content.php
@@ -25,4 +25,7 @@
 	<div class="entry-content">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
+	<div class="read-more-button">
+		<a href="<?php get_permalink(); ?>"><span>Read More <span class="arrow">→</span></span></a>
+	</div>
 </article><!-- #post-## -->
