80e0e14ca1a72a0c64d29a4a1d367095f8230e2f
Evan Gray <evanlawgray@gmail.com>
2017-02-23 12:02:06 -0800

    Remove wp rest api excercise stuff.

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -33,7 +33,7 @@ get_header(); ?>
 
 	      <p> <?php echo $term->description; ?> </p>
 	         
-	      <button><a href='<?php echo $url?>' class='button-link'><?php echo $term->name; ?></a></button>
+	      <a href='<?php echo $url?>' class='button-link'><?php echo $term->name; ?></a>
 
 	    </div>
 
--- themes/inhabitent/functions.php
+++ themes/inhabitent/functions.php
@@ -83,17 +83,23 @@ add_filter( 'stylesheet_uri', 'red_starter_minified_css', 10, 2 );
  * Enqueue scripts and styles.
  */
 function inhabitent_scripts() {
+
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
 
 	wp_enqueue_style( 'font-awesome-cdn', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), '4.7.0');
 
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
+	/*wp_enqueue_scripts( 'jquery' );*/
+
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'inhabitent_scripts' );
+
+
 
 /**
  * Custom template tags for this theme.
