4e00dce3faac9ca7abc7f2ab31c78f5864ba0bee
Evan Gray <evanlawgray@gmail.com>
2017-03-07 12:26:32 -0800

    Refactor and remove & from header and 404 page styles.

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -29,8 +29,7 @@
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?><i class="fa fa-search fa-small" aria-hidden="true"></i></button>
+				<nav id="site-navigation" class="main-navigation" role="navigation" aria-controls="primary-menu" aria-expanded="false">
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					<?php get_search_form(); ?>
 				</nav><!-- #site-navigation -->
